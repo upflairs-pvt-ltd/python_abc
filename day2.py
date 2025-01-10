a554db452ddc337d1f353c1302f9345ebd8ff3dc
Ranjit-singh-786 <jiradhey402@gmail.com>
2025-01-10 08:23:52 +0530

    first commit

---
+++ day2.py
@@ -0,0 +1,57 @@
+# x = 10
+# print(x)
+# print(type(x))
+
+# mohit20 = 100 
+# 20mohit = 100 
+
+# var1  = True 
+# var2 = False 
+# print(var1,var2)
+# print(type(var1))
+# print(type(var2))
+
+# st1 = "upflairs 123 @"
+# st2 = 'upflairs 123 @'
+# print(st1)
+# print(st2)
+# print()
+# print(type(st1),type(st2))
+
+# st = """upflairs pvt ltd jaipur rajasthan """ 
+# st = """upflairs 
+# pvt ltd
+#  jaipur 
+#  rajsthan"""
+# print(st)
+# print(type(st))
+
+st = "upflairs"
+# print(st[-1])   # to access single char from string 
+print(st[::3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
