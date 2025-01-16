fa22be66101e2e4a74065cedd47cbb3d3973e4b7
Ranjit-singh-786 <jiradhey402@gmail.com>
2025-01-16 20:27:35 +0530

    operatorss

---
+++ test.py
@@ -0,0 +1,12 @@
+student_names = ['rohit','mohit','suresh','shaan'] 
+faculty_list = ['radhey','akash','shejal','jai','govind']
+name = "rohit"
+if name in student_names:
+    print("welcome")
+
+print("take a rest")
+
+elif name in faculty_list:
+    print("Most welcome")
+else:
+    print("Get lost")
